f53cf6f8050f486128d4a90628d804569941e13e
Emitor <pengpeng_yu@163.com>
2019-08-30 23:28:38 +0800

    delete file

--- src/main/java/cn/emitor/cheesepro/IConfiguration.java
+++
@@ -1,9 +0,0 @@
-package cn.emitor.cheesepro;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan(basePackages = {"cn.emitor.cheesepro.buff"})
-public class IConfiguration {
-}
